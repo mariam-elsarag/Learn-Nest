ba8becb3be67d44021520b76dcf3631b90d9f334
mariam-elsarag <mariamelsarag44@gmail.com>
2025-06-06 21:55:42 +0200

    learn param

--- src/property/property.controller.ts
+++ src/property/property.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('property')
 export class PropertyController {
@@ -7,6 +7,17 @@ export class PropertyController {
     return 'All properties';
   }
 
+  // with param
+  // method 1
+  // @Get(':id/:slug')
+  // findOne(@Param() parms: { id: string; slug: string }) {
+  //   return parms;
+  // }
+  // method 2
+  @Get(':id/:slug')
+  findOne(@Param('id') id: string, @Param('slug') slug: string) {
+    return `Property ID: ${id}, Slug: ${slug}`;
+  }
   @Post()
   create() {
     return 'will create propperties';
